3578c42029df95c8a06a66c159074e58a1f04cc4
Jamie <jamiecodejob@gmail.com>
2022-06-06 00:33:49 +0800

    create add and remove function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,27 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
   title = 'todo-example';
+
+  allItems = [{ description: 'eat' }, { description: 'sleep' }, { description: 'play' }, { description: 'laugh' }];
+
+  get items() {
+    return this.allItems;
+  }
+
+  addItem(description: string) {
+    this.allItems.unshift({
+      description,
+    });
+  }
+
+  removeItem(item: any) {
+    var index = this.allItems.indexOf(item);
+    if (index !== -1) {
+      this.allItems.splice(index, 1);
+    }
+  }
 }
